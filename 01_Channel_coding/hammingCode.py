d8914f7d56e531bc389fde91647dd698e8529a81
davidcawork <davidcawork@gmail.com>
2020-12-22 20:25:34 +0100

    Added Hamming code

---
+++ 01_Channel_coding/hammingCode.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+
+from scipy import stats
+import numpy as npy
+import math
+
+
+class HammingCode(object):
+
+    """Class to manage Hamming Codes"""
+
+    def __init__(self, q=3):
+        self.q = q
+        self.n = 2**(q) - 1
+        self.k = self.n - q
+
+        self.H, self.G = self.generateMatrix(self.q)
+
+    @staticmethod
+    def generateMatrix(q):
+        """
+
+            - Def:  H = [ Pt | Iq ] ( q x n )
+            - Def:  G = [ Ik |  P ] ( k x n )
+
+        :param q: int (number of redundant digits)
+        :returns: [ numpy.array, numpy.array] (H , G)
+        """
+        n = 2 ** (q) - 1
+        k = n - q
+
+        # First, we have to generate (H) matrix, called parity check matrix.
+        H_Pt = npy.zeros((n - q, q), dtype=int)
+
+        # As we know, q bits will be powers of two.. So we have just to fill H_Pt without them
+        index = 0
+        for i in range(q, n + 1):
+            if not HammingCode.isPower2(i):
+                H_Pt[index] = list(str(bin(i))[2:].zfill(q))
+                index += 1
+
+        # Just to correct order
+        H_Pt = H_Pt.T
+
+        H = npy.hstack((H_Pt, npy.identity(q, dtype=int)))
+
+        # Once we have parity check matrix, we can calculate generator matrix (G)
+        P = H_Pt.T
+        G = npy.concatenate((npy.identity(k, dtype=int), P), axis=1)
+
+        return [H, G]
+
+    @staticmethod
+    def isPower2(number):
+        return (number & (number - 1) == 0) and number != 0
+
+    @staticmethod
+    def zeroPadding(vector, size):
+        return npy.pad(vector, pad_width=(0, size), mode='constant')
+
+    def hammingEncoder(self, bits):
+        encoded_code = []
+        code = npy.array(bits)
+
+        if not code.size % self.k == 0:
+            code = self.zeroPadding(code, self.k - (code.size % self.k))
+
+        for word in npy.split(npy.array(bits), npy.array(bits).size / self.k):
+            pass
+        return npy.hstack(encoded_code)
+
+    def hammingDecoder(self, code):
+        pass
---
+++ 01_Channel_coding/hammingCode_scenario.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+from hammingCode import HammingCode
+from channel import Channel, randBits, getGood
+import numpy as npy
+import random
+import time
+
+
+def scenario(n=3, prob=0.02, frame_len=10000):
+    "Main method to simulate a noisy scenario using Repetition codes"
+
+    # Stats vars
+    start_time = time.time()
+
+    # Scenario behaviour
+    print("Simulating Repetition code scenario...\n")
+    HammingBlock = HammingCode(q=3)
+    bitsTx = randBits(frame_len)
+    code_Tx = HammingBlock.hammingEncoder(bitsTx)
+    # code_Rx = Channel(prob).run(code_Tx)
+    # bitsRx  = HammingBlock.hammingDecoder(code_Rx)
+
+    # Print results
+    print('------------------------------------')
+    print('Time elapsed: '+str((time.time() - start_time)*1000) + ' ms')
+    print('Frame length: '+str(frame_len))
+    print('Repetition factor: '+str(n))
+    print('Error probability: '+str(prob))
+    print('\n------------ Results ---------------')
+    # print('[+] Total bits: '+str(frame_len))
+    # print('[+] Good: '+str(getGood(bitsTx,bitsRx)))
+    # print('[+] Errors: '+str(code_Tx.size - getGood(code_Tx,code_Rx)))
+    # print('[+] Fixed: '+str(code_Rx.size - getGood(code_Rx,npy.repeat(npy.array(bitsRx), n))))
+    # print('[+] Error probability (calculated): '+str((code_Tx.size - getGood(code_Tx,code_Rx))/code_Tx.size))
+
+
+if __name__ == '__main__':
+    scenario(n=3, prob=0.02, frame_len=10)
